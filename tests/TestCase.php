608e7240afd74ae7d5e8badbe2e3964c0c67a7f3
Enea Dhack <enea.so@live.com>
2023-07-22 19:17:12 -0500

    configure tests

---
+++ tests/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author enea dhack <enea.so@live.com>
+ */
+
+declare(strict_types=1);
+
+namespace Vaened\SwiftCart\Tests;
+
+use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+use Vaened\SwiftCart\SwiftCartConfig;
+use Vaened\SwiftCart\Tests\Utils\MoneyFactory;
+
+abstract class TestCase extends PhpUnitTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setUpConfig();
+    }
+
+    private function setUpConfig(): void
+    {
+        SwiftCartConfig::setDefaultCurrency(MoneyFactory::defaultCurrency());
+        SwiftCartConfig::setDefaultContext(MoneyFactory::defaultContext());
+    }
+}
+
