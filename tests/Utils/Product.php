c38e4417e6af29a95155a5c9ed2e06f747432388
Enea Dhack <enea.so@live.com>
2023-08-04 16:43:24 -0500

    test: make product adjustable

--- tests/Utils/Product.php
+++ tests/Utils/Product.php
@@ -8,19 +8,29 @@ declare(strict_types=1);
 namespace Vaened\SwiftCart\Tests\Utils;
 
 use Vaened\PriceEngine\Adjustments\Adjusters;
+use Vaened\PriceEngine\Adjustments\Charge;
+use Vaened\PriceEngine\Adjustments\Discount;
 use Vaened\PriceEngine\Money\Amount;
 use Vaened\SwiftCart\Entities\Chargeable;
 use Vaened\SwiftCart\Entities\Discountable;
 use Vaened\SwiftCart\Entities\Tradable;
 use Vaened\SwiftCart\UniqueId;
 
+use function Lambdish\Phunctional\each;
+
 final class Product implements Tradable, Discountable, Chargeable
 {
+    private readonly Adjusters $charges;
+
+    private readonly Adjusters $discounts;
+
     public function __construct(
         private readonly string $id,
         private readonly Amount $amount,
     )
     {
+        $this->charges   = Adjusters::empty();
+        $this->discounts = Adjusters::empty();
     }
 
     public static function random(): self
@@ -31,6 +41,19 @@ final class Product implements Tradable, Discountable, Chargeable
     public static function create(Amount $amount): self
     {
         return new self(UniqueId::random()->uniqueId(), $amount);
+    }
+
+    public function with(Discount|Charge ...$adjusters): self
+    {
+        each(function (Discount|Charge $adjuster) {
+            if ($adjuster instanceof Discount) {
+                $this->discounts->push($adjuster);
+            } else {
+                $this->charges->push($adjuster);
+            }
+        }, $adjusters);
+
+        return $this;
     }
 
     public function uniqueId(): string
@@ -45,11 +68,11 @@ final class Product implements Tradable, Discountable, Chargeable
 
     public function charges(): Adjusters
     {
-        return Adjusters::empty();
+        return $this->charges;
     }
 
     public function discounts(): Adjusters
     {
-        return Adjusters::empty();
+        return $this->discounts;
     }
 }
